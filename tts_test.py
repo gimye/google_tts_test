c82f24d8c48736b1d584b75ab9a6b01c14fab03f
gimye <gimyedev@gmail.com>
2024-11-27 00:16:19 +0900

    first commit

---
+++ tts_test.py
@@ -0,0 +1,30 @@
+from google.cloud import texttospeech
+from google.oauth2 import service_account
+
+def synthesize_text():
+
+    text = "구글 tts 테스트 코드입니다."
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.VoiceSelectionParams(
+    language_code="ko-KR", name="ko-KR-Standard-B", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+synthesize_text()
